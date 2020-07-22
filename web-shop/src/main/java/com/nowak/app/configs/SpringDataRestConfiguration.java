7d1afbe87f6f9bfedc82bf47d11bff38edbf7533
KamilaNowak <knowak242@gmail.com>
2020-07-22 22:30:08 +0200

    update data rest config

--- web-shop/src/main/java/com/nowak/app/configs/SpringDataRestConfiguration.java
+++ web-shop/src/main/java/com/nowak/app/configs/SpringDataRestConfiguration.java
@@ -2,14 +2,30 @@ package com.nowak.app.configs;
 
 import com.nowak.app.entities.Item;
 import com.nowak.app.entities.ItemCategory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.support.ConfigurableConversionService;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.http.HttpMethod;
 
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.EntityType;
+import javax.persistence.metamodel.Type;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Configuration
 public class SpringDataRestConfiguration implements RepositoryRestConfigurer {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public SpringDataRestConfiguration(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
 
     private final HttpMethod[] disabledHttpMethods = {
             HttpMethod.POST,
@@ -32,6 +48,23 @@ public class SpringDataRestConfiguration implements RepositoryRestConfigurer {
                         httpMethods.disable(disabledHttpMethods)))
                 .withCollectionExposure(((metdata, httpMethods) ->
                         httpMethods.disable(disabledHttpMethods)));
+
+        displayCategoryId(configuration);
+    }
+
+    /*
+    this fynction adds self id for every category entity in response
+     */
+    private void displayCategoryId(RepositoryRestConfiguration configuration) {
+        Set<EntityType<?>> entityTypeSet = entityManager
+                .getMetamodel()
+                .getEntities();
+
+        List<Class<?>> list = entityTypeSet.stream()
+                .map(Type::getJavaType)
+                .collect(Collectors.toList());
+        Class[] types = list.toArray(new Class[0]);
+        configuration.exposeIdsFor(types);
     }
 
     @Override
--- web-shop/src/main/java/com/nowak/app/repositories/ItemRepository.java
+++ web-shop/src/main/java/com/nowak/app/repositories/ItemRepository.java
@@ -1,9 +1,16 @@
 package com.nowak.app.repositories;
 
 import com.nowak.app.entities.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @CrossOrigin(origins = "*")
 public interface ItemRepository extends PagingAndSortingRepository<Item, Long> {
+
+    //http://localhost:8080/items/search/findByItemCategory_Id?id=1
+    Page<Item> findByItemCategory_Id(@RequestParam("id") Long id, Pageable pageable);
 }
